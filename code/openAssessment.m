1ae86c07e3d2fa650d30bea60f52c66167a6a71e
Adam Taylor <at895@york.ac.uk>
2016-03-24 14:26:48 +0000

    Create indexing string from a configuration

--- code/openAssessment.m
+++ code/openAssessment.m
@@ -11,11 +11,12 @@ initialConditionalProbabilities = 0.5; % Value for the initial probabilities
 % subsequent columns
 CPT = cell(1,size(bncsv,2));
 
+
+% Initialise CPT's with initial conditional probabilities
 for column = bncsv; % For each column in the csv (node in the DAG)
     % For each 1 in the column, we have an edge from that node, so we
     % generate its CPT
-    CPT{csvColumnIndex} = initialConditionalProbabilities;
-    CPTIndex = 2; % Keep track of the Column in the CPT page we are storing data in
+
     numberOfParents = numel(find(column==1)); % Get the number of parents the node has
     binaryPermutations = dec2bin(0:2^numberOfParents-1); % Compute the binary permutations for this number
     for permutation = 1:size(binaryPermutations,1) % For each possibility
@@ -25,6 +26,7 @@ for column = bncsv; % For each column in the csv (node in the DAG)
     csvColumnIndex = csvColumnIndex + 1;
 end
 
+% E-Step
 for row = 1:size(datacsv,1) % For each data point
     global configurations;
     configurations = [];
@@ -32,7 +34,16 @@ for row = 1:size(datacsv,1) % For each data point
     for configuration = 1:size(configurations,1) % For each configuration
         for variable = 1:numel(configurations(configuration,:)) % For each variable in the config
            parents = find(bncsv(:,variable)); % Find the parents of the variable
-           
+           configString = '';
+           if size(parents,1) == 0 %Variable has no parents
+               configString = '0';
+           else
+               for parent = 1:size(parents,1) % Get the current value of all parents to build a binary string to be used for CPT indexing
+                   configString = strcat(configString, int2str(configurations(configuration,parents(parent))));
+               end
+           end
+           columnIndex = bin2dec(configString) + 1;
+
         end
     end
 end
