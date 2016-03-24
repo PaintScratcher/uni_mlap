714c7c609d41c81b405d3cbc9160adadd6e246af
Adam Taylor <at895@york.ac.uk>
2016-03-24 09:49:00 +0000

    Change CPT's to take probabilities rather than data and start data point loop

--- code/openAssessment.m
+++ code/openAssessment.m
@@ -5,6 +5,7 @@ bncsv = csvread(bncsvName);
 datacsv = csvread(datacsvName);
 
 csvColumnIndex = 1; % Keep track of what column in the csv we are reading
+initialConditionalProbabilities = 0.5;
 
 
 % Create our CPT Data structure, with a page for each node in the network,
@@ -16,14 +17,19 @@ CPT = cell(1,size(bncsv,2));
 for column = bncsv; % For each column in the csv (node in the DAG)
     % For each 1 in the column, we have an edge from that node, so we add
     % its data to our CPT
-    CPT{csvColumnIndex} = datacsv(:,csvColumnIndex); % Add the observed data for this node to the CPT
+    %CPT{csvColumnIndex} = datacsv(:,csvColumnIndex); % Add the observed data for this node to the CPT
+    CPT{csvColumnIndex} = initialConditionalProbabilities;
     CPTIndex = 2; % Keep track of the Column in the CPT page we are storing data in
     for indx = 1:numel(column) % For each element in the column
         if column(indx) == 1 % Check if we have an incoming edge from that node
-            CPT{csvColumnIndex} = [CPT{csvColumnIndex} datacsv(:,indx)];
+           % CPT{csvColumnIndex} = [CPT{csvColumnIndex} datacsv(:,indx)];
+           CPT{csvColumnIndex}(:,indx) = initialConditionalProbabilities; 
             CPTIndex = CPTIndex +1;
         end
     end
-    csvColumnIndex = csvColumnIndex + 1;   
-    
+    csvColumnIndex = csvColumnIndex + 1;
+end
+
+for row = 1:size(datacsv,1)
+    row = datacsv(row,:);
 end
