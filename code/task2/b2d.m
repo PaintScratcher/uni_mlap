91e4026d33111e042856217248b2a40280c14523
adam <hvfcrazy@hotmail.com>
2016-05-04 00:29:45 +0100

    Final

--- code/task2/b2d.m
+++ code/task2/b2d.m
@@ -1,9 +1,5 @@
 function y = b2d(x)
-%http://www.mathworks.com/matlabcentral/fileexchange/26447-efficient-convertors-between-binary-and-decimal-numbers
-% Convert a binary array to a decimal number
-% 
-% Similar to bin2dec but works with arrays instead of strings and is found to be 
-% rather faster
-
+% Function to convert a binary array to a decimal number
+% Adapted from http://www.mathworks.com/matlabcentral/fileexchange/26447-efficient-convertors-between-binary-and-decimal-numbers
 z = 2.^(length(x)-1:-1:0);
 y = sum(x.*z);
